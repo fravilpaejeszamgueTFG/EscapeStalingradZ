b0eae7b27c803d39df356947f03554e1a8d1a3e6
Francis Villalobos <72505094+fravilpae@users.noreply.github.com>
2024-04-24 22:02:58 +0200

    Update PlayerCharacter.cpp

--- Source/EscapeStalingradZ/character/PlayerCharacter.cpp
+++ Source/EscapeStalingradZ/character/PlayerCharacter.cpp
@@ -326,7 +326,6 @@ int APlayerCharacter::GetNumberOfHitModifiersAttack(AZombie* zombie)
 	if (!useReadyWeapon) {
 		res += 2;
 	}
-	
 	if (exp >= 10 && useReadyWeapon && readyWeapon->weaponName == PreferredWeapon) {
 		res--;
 	}
